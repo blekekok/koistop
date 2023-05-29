4709744aee837e4e2f77ddc6e92aeabbc54f8e52
hehe <jasonwillcond@gmail.com>
2023-05-29 19:46:46 +0700

    header slese

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
     import { Button } from 'flowbite-svelte';
 </script>
 
-<div class="h-screen w-full bg-no-repeat text-white" style="background-image:url('{img}');background-color: rgba(0, 0, 0, 0.6)">
-    <div style="background-color: rgba(0, 0, 0, 0.6);">
+<div class="h-screen w-full bg-no-repeat text-white" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('{img}');">
+    <div>
         <h1>Welcome to SvelteKit</h1>
         <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
         <Button color="blue">Hello</Button>
