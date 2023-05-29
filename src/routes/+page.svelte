428d1011d58797c3cdf458dc749124d97aa5af43
hehe <jasonwillcond@gmail.com>
2023-05-29 19:31:57 +0700

    header selesai

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
+    import img from '$lib/images/ikan1.jpg';
     import { Button } from 'flowbite-svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Button color="blue">Hello</Button>
+<div class="h-screen w-full bg-no-repeat text-white" style="background-image:url('{img}');background-color: rgba(0, 0, 0, 0.6)">
+    <div style="background-color: rgba(0, 0, 0, 0.6);">
+        <h1>Welcome to SvelteKit</h1>
+        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+        <Button color="blue">Hello</Button>
+    </div>
+</div>
 
 <style>
 </style>
