a4635efc56afa2ba943aacc16a3a73cea2b743af
hehe <jasonwillcond@gmail.com>
2023-05-29 14:19:04 +0700

    new logo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css";
+    import img from '$lib/images/logoikangud.svg';
+    import img2 from '$lib/images/logoikan.svg';
+</script>
+  
+<header>
+    <div class="flex">
+        <a href="/"><img class="h-[75px]" src={img} alt=""></a>
+    </div>
+</header>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script>
+    import { Button } from 'flowbite-svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button color="blue">Hello</Button>
+
+<style>
+</style>
