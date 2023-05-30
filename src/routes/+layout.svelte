cd3da5963025644dba5db6b1951bb4987cdc91a2
hehe <jasonwillcond@gmail.com>
2023-05-30 14:05:50 +0700

    header sudah diperbaiki searchbarnya  sama sticky background homepagenya

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,8 @@
 <script>
+    import { Search, Button } from 'flowbite-svelte'
+    function handleVoiceBtn() {
+        alert('You clicked voice button');
+    }
     import "../app.css";
     import img from '$lib/images/logoikangud.svg';
     import img2 from '$lib/images/logoikan.svg';
@@ -6,31 +10,34 @@
     const menus = [
         {
             title: 'FISH',
-            href: '/'
+            href: '/fish'
         },
         {
             title: 'UTILITY',
-            href: '/browse'
+            href: '/utility'
         },
         {
             title: 'CART',
-            href: '/browse'
+            href: '/cart'
         },
         {
             title: 'SELL',
-            href: '/browse'
+            href: '/sell'
         },
         {
             title: 'LOGIN',
-            href: '/browse'
+            href: '/login'
         }
     ];
 </script>
 
-<main class="w-full h-full bg-black">
-    <header class="flex justify-between bg-black/50 w-full h-20 p-2 sticky top-0 px-2 py-4 text-lg md:px-0 text-white ">
+
+<main class="w-full h-full bg-black text-white">
+    <header class="flex justify-between bg-black/50 w-full h-20 p-2 sticky top-0 text-lg md:px-0 text-white z-10">
         <a href="/" class="ml-10 mr-6"><img class="h-full invert" src={img} alt=""></a>
-        <form class="flex-grow mx-10">   
+
+        <!-- Another button style g apus buat jaga jaga aj -->
+        <!-- <form class="flex-grow mx-10">   
             <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative items-center">
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -39,7 +46,15 @@
                 <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type here! :)" required>
                 <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-gray-500 hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
-        </form>
+        </form> -->
+
+        <div class="flex items-center gap-2 place-content-between w-2/5">
+            <Search class="py-1.5 flex gap-2 items-center" placeholder="Search Mockups, Logos, Design Templates..."></Search>
+            <Button>
+                <svg class="w-5 h-5 mr-2 -ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/></svg>
+                Search
+            </Button>            
+        </div>
     
 
         <ul class="flex h-full items-center gap-2 font-bold font-sans text-lg">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,38 @@
 <script>
-    import img from '$lib/images/ikan1.jpg';
+    import img1 from '$lib/images/ikan1.jpg';
+    import img2 from '$lib/images/ikan2.jpg';
+    import img3 from '$lib/images/ikan3.jpg';
     import { Button } from 'flowbite-svelte';
+    import { bounceInOut } from 'svelte/easing';
+    import { Carousel, CarouselTransition } from 'flowbite-svelte'
+    export const images = [
+    {
+        id: 0,
+        name: "koi1",
+        imgurl: img1,
+        attribution: "ikan koi",
+    },
+    {
+        id: 1,
+        name: "koi2",
+        imgurl: img2,
+        attribution: "ikan koi2",
+    },
+    {
+        id: 2,
+        name: "koi3",
+        imgurl: img3,
+        attribution: "ikan koi3",
+    },
+    ];  
 </script>
 
-<div class="h-screen w-full bg-no-repeat text-white" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('{img}');">
-    <div>
-        <h1>Welcome to SvelteKit</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-        <Button color="blue">Hello</Button>
+<div class="h-screen w-full bg-no-repeat text-white" style="background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('{img1}');background-attachment:fixed">
+    <div class="flex justify-center w-full py-10">    
+        <!-- <h1>dsadasdas</h1> -->
+        <div class="max-w-4xl">
+            <CarouselTransition {images} transitionType="slide" transitionParams="{{duration: 1500, easing: bounceInOut}}" showCaptions={false} showThumbs={false}/>
+        </div>
     </div>
 </div>
 
