a985c6b747f6bfda496e12acbd6593b572feb8c3
hehe <jasonwillcond@gmail.com>
2023-06-13 12:30:21 +0700

    fix image size

--- src/lib/components/ItemListPage.svelte
+++ src/lib/components/ItemListPage.svelte
@@ -40,13 +40,15 @@
         {#each items as item}
         <a href="/{type}/{item.id}">
             <div class="flex flex-col w-56 p-1 rounded-md gap-1 bg-gray-600/70">
-                <img class="object-contain w-full h-full rounded-md" src="{item.image || fishImage}" alt="">
+                <div class="h-32 overflow-hidden rounded-md">
+                    <img class="object-cover w-full h-full" src="{item.image || fishImage}" alt="">
+                </div>
                 <div class="rounded-md px-1">
                     <h2 class="font-light">{item.name}</h2>
                     <h3 class="font-semibold">Rp. {numberFormat.format(item.price)}</h3>
                     <h4 class="font-light">{item.seller.username}</h4>
                 </div>
-            </div>
+            </div>      
         </a>
         {/each}
       </div>
