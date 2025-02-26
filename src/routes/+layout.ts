3b074881bf8b38a9dadc82881f75b7a0b4949db1
jtbernas <jtbernas@gmail.com>
2025-02-27 00:16:04 +0700

    update

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,6 +2,8 @@ import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/publi
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
